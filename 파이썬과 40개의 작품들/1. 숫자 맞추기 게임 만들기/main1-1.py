3fdb1efb2f07140413545f1da404e9cb04ef3daf
CHOIisaac <ehdrhkdls@gmail.com>
2022-12-10 22:36:28 +0900

    에러방지

--- 파이썬과 40개의 작품들/1. 숫자 맞추기 게임 만들기/main1-1.py
+++ 파이썬과 40개의 작품들/1. 숫자 맞추기 게임 만들기/main1-1.py
@@ -7,13 +7,16 @@ random_number = random.randint(1, 100)
 game_count = 1
 
 while True:
-    my_number = int(input("1~100 사의 숫자를 입력하세요: "))
+    try:
+        my_number = int(input("1~100 사의 숫자를 입력하세요: "))
 
-    if my_number > random_number:
-        print("다운")
-    elif my_number < random_number:
-        print("업")
-    elif my_number == random_number:
-        print(f"축하{game_count}번만에 맞춤")
-        break
-    game_count = game_count + 1
+        if my_number > random_number:
+            print("다운")
+        elif my_number < random_number:
+            print("업")
+        elif my_number == random_number:
+            print(f"축하{game_count}번만에 맞춤")
+            break
+        game_count = game_count + 1
+    except:
+        print("에러발생 숫자를 입력 바람.")
