ef32fd72bbd497dcedec07c9e292ff3ad0c991d2
CHOIisaac <ehdrhkdls@gmail.com>
2022-12-11 23:47:13 +0900

    study

---
+++ 파이썬과 40개의 작품들/3.텍스트를 음성으로 변환/main3-2.py
@@ -0,0 +1,12 @@
+import os
+
+from playsound import playsound
+from gtts import gTTS
+
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+text = "안녕하세요. 파이썬과 40개의 작품들 입니다."
+
+tts = gTTS(text=text, lang='ko')
+tts.save("hi.mp3")
+playsound("hi.mp3")
