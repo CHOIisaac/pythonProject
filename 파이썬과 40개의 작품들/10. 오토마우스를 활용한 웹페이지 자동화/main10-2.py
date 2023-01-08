3a29213134843343f10111c39c917edccf256042
CHOIisaac <ehdrhkdls@gmail.com>
2023-01-08 22:20:12 +0900

    study

---
+++ 파이썬과 40개의 작품들/10. 오토마우스를 활용한 웹페이지 자동화/main10-2.py
@@ -0,0 +1,14 @@
+import time
+import pyautogui
+import pyperclip
+
+# pyautogui.moveTo(1241, 206, 0.2)
+# pyautogui.click()
+time.sleep(0.5)
+
+pyperclip.copy("서울 날씨")
+pyautogui.hotkey("ctrl", "v")
+time.sleep(0.5)
+
+pyautogui.write(["enter"])
+time.sleep(1)
